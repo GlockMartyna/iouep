10e4f7764808af282a9106677d577c81db138829
Martyna Glock <90167@student.ue.poznan.pl>
2023-04-16 17:39:56 +0200

    added romb

--- ex_2.py
+++ ex_2.py
@@ -1,3 +1,4 @@
+import math
 # trojkat
 a = 10
 b = 12
@@ -8,3 +9,9 @@ pole = 1/2*a*h
 obwod = a+b+c
 
 print("Pole trojkata wynosi " +  str(pole) + ",a obwod wynosi "+str(obwod))
+
+#romb
+
+obw_romb = 4*a
+pole_romb = a*h
+
