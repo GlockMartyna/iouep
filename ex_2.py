e4f5a03888deb8364834bc7429c495cda3f26aa1
Martyna Glock <90167@student.ue.poznan.pl>
2023-04-16 17:51:45 +0200

    added prints

--- ex_2.py
+++ ex_2.py
@@ -14,22 +14,23 @@ print("Pole trojkata wynosi " +  str(pole) + ",a obwod wynosi "+str(obwod))
 
 obw_romb = 4*a
 pole_romb = a*h
-
+print("Pole wynosi " +  str(pole_romb) + ",a obwod wynosi "+str(obw_romb))
 #kolo
 
 r = 8
 
 obw_kolo = 2*math.pi*r
 pole_kolo = math.pi*r**2
-
+print("Pole wynosi " +  str(pole_kolo) + ",a obwod wynosi "+str(obw_kolo))
 #trapez
 
 d=14
 
 obw_trapez = a+b+c+d
 pole_trapez = 1/2*(a+b)*h
-
+print("Pole wynosi " +  str(pole_trapez) + ",a obwod wynosi "+str(obw_trapez))
 #prostokat
 obw_prostokat = 2*a+2*b
 pole_prostokat = a*b
 
+print("Pole wynosi " +  str(pole_prostokat) + ",a obwod wynosi "+str(obw_prostokat))
