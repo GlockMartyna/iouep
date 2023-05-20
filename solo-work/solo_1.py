418d5903be79f1ee8bf200cb873c6fbc3da2e75e
Martyna Glock <90167@student.ue.poznan.pl>
2023-05-20 22:38:13 +0200

    Zadanie 1

---
+++ solo-work/solo_1.py
@@ -0,0 +1,11 @@
+#zadanie 1
+hello = "hello"
+student =" Ola"
+
+
+print(f"{hello}{student}".format(hello,student) )
+
+#zadanie 2
+#hello = "hello"
+#student= "hello" + input("Martyna")
+#print(student)
