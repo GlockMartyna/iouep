4d20869a8d48ffb82387f075ab5bad071d5e7cd4
Martyna Glock <90167@student.ue.poznan.pl>
2023-07-09 18:02:16 +0200

    Zadanie 3-8

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,3 +1,46 @@
+#zadanie 9
+studenci5 = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+liczba_n=0
+
+
+
+#zadanie 8
+
+studenci4 = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+x= sorted(sorted(studenci4), key=lambda s: s.split()[1])
+print(x)
+
+#zadanie 7
+studenci3 = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+posortowani_studenci=sorted(studenci3)
+print("Alfabetyczna lista studentow wynosi: ", posortowani_studenci)
+
+
+#zadanie 6
+ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
+liczba_nawiasow=ciag_znakow.count("(")
+print(print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow)))
+
+#zadanie 5
+
+liczba=3
+potega=4
+wynik = liczba**potega
+print("Wynik wynosi",wynik)
+
+#zadanie 4
+
+studenci2 = ["Ania", "Kasia", "Piotr", "Tomek"]
+
+for i in studenci2:
+    print("hello",i)
+
+#zadanie 3
+l_studenci=0
+studenci=["Ania", "Kuba", "Piotr", "Jan"]
+l_studenci= len(studenci)
+print("Liczba studentów wynosi",l_studenci)
+
 #zadanie 1
 hello = "hello"
 student =" Ola"
@@ -8,4 +51,16 @@ print(f"{hello}{student}".format(hello,student) )
 #zadanie 2
 
 student= input("Hello Martyna")
-print()
+print(student)
+
+
+
+
+
+
+
+
+#zadanie 7
+#zadanie 8
+#zadanie 9
+#zadanie 10
